0bf5f0136531ee0237fff6e3d6f01f2fc1117df9
Celal Ergun <celalergun@gmail.com>
2022-07-18 09:27:05 +0300

    Comments moved

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -27,10 +27,10 @@ function setEvents() {
                 // if it is starting with 'MZ' (Mark Zbikowski) 
                 mzSign = array[0] * 0x100 + array[1];
                 var peLocation = array[0x3c] + (array[0x3d] * 0x100) + (array[0x3e] * 0x10000) + (array[0x3f] * 0x1000000);
-                // if it has MZ and PE marks (http://www.phreedom.org/research/tinype/)
                 peSign = (array[peLocation] << 24) + (array[peLocation + 1] << 16) + (array[peLocation + 2] << 8) + (array[peLocation + 3]);
             }
             //console.log(peSign.toString(16));
+            // if it has MZ and PE marks (http://www.phreedom.org/research/tinype/)
             if (isBigEnough && mzSign == 0x4d5a && peSign == 0x50450000) {
                 document.getElementById('ctype').innerText = "File is PE";
                 document.getElementById('btnSubmit').disabled = false;

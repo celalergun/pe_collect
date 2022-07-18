e1335a5951c25545c2a54364abea47d8abbf54e7
Celal Ergun <celalergun@gmail.com>
2022-07-18 17:31:40 +0300

    SQLite support added, file saving and querying implemented

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const fs = require('fs');
-const multer = require('multer');
 const path = require('path');
+//file upload module
+const multer = require('multer');
+const upload = multer({preservePath:true});
+
 var app = express();
 app.use(express.static(path.join(process.cwd(), '/public')));
 //app.use(express.json);
 const PORT = 3003;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 // let's create the database if it does not exist
 const db = require('better-sqlite3')('./database/pe_metadata.db3', { verbose: console.log });
 db.exec('Create Table If Not Exists meta_data(id integer primary key autoincrement, hash varchar(64) not null, filename text not null, filesize int64, directory text not null);');
 // and add an index to make the queries faster
 db.exec('Create Unique Index If Not Exists ByHash on meta_data(hash);');
 db.close();
-
-//file upload module
-const upload = multer({dest: 'storage/'});
 
 app.get('/', (req, res) => {
     res.setHeader('Content-type', 'text/html');
@@ -28,32 +30,46 @@ app.get('/', (req, res) => {
 
 app.post('/hash', (req, res) => {
     const hash = req.body.hash;
-    let fileName = path.join(process.cwd(), 'storage', hash);
+    let fileName = path.join(process.cwd(), 'storage', hash+'.sample');
     const db = require('better-sqlite3')('./database/pe_metadata.db3', { verbose: console.log });
-    var query = db.prepare('select id from meta_data where hash = ?');
-    const val = query.get(hash);
-    if (val){
+    var query = db.prepare('select id from meta_data where hash = ?').bind(hash);
+    const val = query.get();
+    if (val) {
         res.status(200).send(
             {
                 exists: true,
                 status: "You don't need to send this file"
             });
-    
+
     } else {
         res.status(200).send(
             {
                 exists: false,
                 status: "Ready to send"
             });
-    
+
     }
+    db.close();
 });
 
-app.post('/upload', (req, res) => {
-    if (req.file) {
-        console.log(req.files);
+app.post('/upload', upload.single('sample'), function (req, res, next) {
+    let hash = req.body.hashHex;
+    let fileName = req.file.originalname;
+    let fileSize = req.file.size;
+    let directory = req.body.filePath;
+    let physicalFileName = path.join(process.cwd(), 'storage', hash+ '.sample');
+    if (hash === undefined || fileName === undefined || fileSize === undefined || directory === undefined){
+        res.status(403).send('Invalid data');
+        res.end();
+        return;
     }
-    res.end();
+    fs.writeFileSync(physicalFileName, req.file.buffer);
+    const db = require('better-sqlite3')('./database/pe_metadata.db3', { verbose: console.log });
+    var s = 'insert into "meta_data" (hash, filename, filesize, directory) values (?, ?, ?, ?);';
+    var query = db.prepare(s);
+    var result = query.run(hash, fileName, fileSize, directory);
+    console.log(result);
+    db.close();
 });
 
 app.get("*", (req, res) => {

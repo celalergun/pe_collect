fb14ef505475021f90a042b53b72e836dd714e99
Celal Ergun <celalergun@gmail.com>
2022-07-18 12:11:24 +0300

    Hash post query method added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 var app = express();
 app.use(express.static(path.join(process.cwd(), '/public')));
+//app.use(express.json);
 const PORT = 3003;
 
 app.get('/', (req, res) => {
@@ -14,6 +15,14 @@ app.get('/', (req, res) => {
     res.end();
 });
 
+app.post('/hash', (req, res) => {
+    res.status(200).send(
+        {
+            exists: false,
+            status: "Ready to send"
+        });
+});
+
 app.post('/upload', (req, res) => {
     if (req.files){
         console.log(req.files);
@@ -23,8 +32,8 @@ app.post('/upload', (req, res) => {
 
 app.get("*", (req, res) => {
     res.setHeader('Content-type', 'text/html');
-    indexFile = fs.readFileSync(path.join(process.cwd(), 'notfound.html'));
-    res.write(indexFile, (err) => {
+    file404 = fs.readFileSync(path.join(process.cwd(), 'notfound.html'));
+    res.write(file404, (err) => {
         if (err) throw err;
     });
     res.end();
